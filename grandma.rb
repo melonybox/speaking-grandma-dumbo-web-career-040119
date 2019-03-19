f1a0d32bd81522a1b0e487602f2e2badd18cd386
Gino Desangles <blushingbox@yahoo.com>
2019-03-19 01:17:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
+   elsif phrase == "Hi Nana, how are you?"
+   "HUH?! SPEAK UP, SONNY!"
+   
  end
 end
